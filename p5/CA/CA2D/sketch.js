9e291b37b7f0470bdeaf5c1b0ce0a49a6979b764
magicbrush <magicbrush.lv@gmail.com>
2019-04-10 18:44:59 +0800

    CA almost complete

--- p5/CA/CA2D/sketch.js
+++ p5/CA/CA2D/sketch.js
@@ -1,11 +1,11 @@
 var C; // cells
 
 var lastTime = 0;
-var colNum = 15;
-var rowNum = 15;
-var gap = 20;
+var colNum = 32;
+var rowNum = 32;
+var gap = 10;
 
-var timeInterval = 1.0;
+var timeInterval = 0.1;
 
 // 函数setup() ：准备阶段
 function setup() 
@@ -26,13 +26,14 @@ function setup()
 			{
 				C[i][j] = 1;
 			}
+			C[i][j] = random(0,1)>0.9?1:0;
 		}
 	}
 }
 
 // 函数draw()：作画阶段
 function draw() {
-	fill(255,1);
+	fill(255,255,255,255);
 	rect(0,0,width,height);
 
 	var tNow = getTime(); // 当前时刻
@@ -43,14 +44,45 @@ function draw() {
 		{
 			for(var j=0;j<rowNum;j++)
 			{
-				C[i][j] = random(0,1)>0.5?0:1;
+				var sum = 0;
+				var iternum = 0
+				for(var k=-1;k<=1;k++)
+				{
+					for(var l=-1;l<=1;l++)
+					{
+						var id0 = clampId((i+k),colNum);
+						var id1 = clampId((j+l),rowNum);
+					
+						var value = C[id0][id1];
+						sum += value;
+						iternum ++;
+					}
+				}
+				print("iterNum:" + iternum + " sum:"+sum);
+				var valNow = C[i][j];
+				var isAlive = (valNow===1);
+				if(isAlive)
+				{
+					if(sum==1||sum>=4)
+					{
+						C[i][j] = 0;
+					}
+					//C[i][j] = 0;
+				}
+				else
+				{
+					if(sum==1)
+					{
+						C[i][j] = 1;
+					}
+					//C[i][j] = 1;
+				}
+
 			}
 		}
 		lastTime = tNow;
 	}
 
-	
-	
 	var xGap = width/colNum;
 	var yGap = height/rowNum;
 	for(var i =0;i<colNum;i++)
@@ -62,11 +94,11 @@ function draw() {
 			scale(xGap*0.9,yGap*0.9);
 			if(C[i][j]===1)
 			{
-				fill(255);
+				fill(0);
 			}
 			else
 			{
-				fill(0);
+				fill(255);
 			}
 			stroke(0);
 			strokeWeight(0.05);
@@ -77,6 +109,18 @@ function draw() {
 
 }
 
+function clampId(id, maxId)
+{
+	if(id<0)
+	{
+		id= maxId-1;
+	}
+	else if(id>=maxId)
+	{
+		id = 0;
+	}
+	return id;
+}
 
 function getTime()
 {
